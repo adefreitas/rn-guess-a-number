f6a5ac9a951f6c8e5db0509252b614f235d52a40
Andres De Freitas <adefreitas12@gmail.com>
2020-04-13 15:07:02 +0100

    improve previous guesses list

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
-import {View, Text, StyleSheet, Alert} from 'react-native';
+import { View, Text, StyleSheet, Alert, FlatList, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
 import MainButton from '../components/MainButton';
+import BodyText from '../components/BodyText';
 
 const generateRandomBetween = (min: number, max: number, exclude: number): number => {
   min = Math.ceil(min);
@@ -18,25 +19,23 @@ const generateRandomBetween = (min: number, max: number, exclude: number): numbe
 
 export default function GameScreen(this: any, props: any) {
   const {userChoice, onGameOver} = props;
-  const [currentGuess, setCurrentGuess] = useState(
-    generateRandomBetween(1, 100, userChoice),
-  );
-  
-  const [currentRounds, setCurrentRounds] = useState(0);
+  const initialGuess = generateRandomBetween(1, 100, userChoice);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+  const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
 
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
 
   useEffect(() => {
     if (currentGuess === userChoice) {
-      onGameOver(currentRounds);
+      onGameOver(pastGuesses.length);
     }
   }, [currentGuess, userChoice, onGameOver]);
 
   const nextGuessHandler = (direction: string) => {
     if (
-      (direction === 'lower' && (currentGuess < userChoice || currentGuess === 1)) ||
-      (direction === 'greater' && (currentGuess > userChoice || currentGuess === 99))
+      (direction === 'lower' && currentGuess < userChoice) ||
+      (direction === 'greater' && currentGuess > userChoice)
     ) {
       Alert.alert(
         'That\'s impossible!',
@@ -49,12 +48,18 @@ export default function GameScreen(this: any, props: any) {
     if (direction === 'lower') {
       currentHigh.current = currentGuess;
     } else {
-      currentLow.current = currentGuess;
+      currentLow.current = currentGuess + 1;
     }
-    setCurrentGuess(
-      generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
-    );
-    setCurrentRounds(currentRounds+1);
+    const newGuess = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess); 
+    setCurrentGuess(newGuess);
+    setPastGuesses([newGuess.toString(), ...pastGuesses])
+  };
+
+  const renderListItem = (listLength: number, itemData: any) => {
+    return <View style={styles.listItem}>
+      <BodyText>Guess number {listLength - itemData.index}</BodyText>
+      <BodyText>{itemData.item}</BodyText>
+    </View>
   };
 
   return (
@@ -65,6 +70,16 @@ export default function GameScreen(this: any, props: any) {
         <MainButton onPress={nextGuessHandler.bind(this, 'lower')}><Ionicons size={24} name='md-remove'/></MainButton>
         <MainButton onPress={nextGuessHandler.bind(this, 'greater')}><Ionicons size={24} name='md-add'/></MainButton>
       </Card>
+      <View style={styles.listContainer}>
+        <ScrollView contentContainerStyle={styles.list}>
+          <FlatList
+            keyExtractor={(item) => item}
+            data={pastGuesses}
+            renderItem={renderListItem.bind(this, pastGuesses.length)}
+            contentContainerStyle={styles.list}
+          ></FlatList>
+        </ScrollView>
+      </View>
     </View>
   );
 };
@@ -80,5 +95,23 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     width: 300,
     maxWidth: '80%',
+  },
+  listItem: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 15,
+    marginVertical: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%'
+  },
+  listContainer: {
+    width: '60%',
+    flex: 1,
+  },
+  list: {
+    flexGrow: 1,
+    justifyContent: 'flex-end'
   }
 });
